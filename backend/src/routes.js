e6e7e2f68386e83e469573a969bbd1a651fa58ff
Eduardo Lima <eduardolima.df@gmail.com>
2019-08-01 18:54:25 -0300

    Adding controllers, config, likes and upload logic

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const multer = require('multer');
+const PostController = require('./controllers/PostController');
+const uploadConfig = require('./config/upload');
+const LikeController = require('./controllers/LikeController');
+
 const router = new express.Router();
+const upload = multer(uploadConfig);
 
 router.get('/', (req, res) => {
-  return res.send(`Hello ${req.query.name}`)
+  return res.send("Página inicial")
 })
 
+router.post('/posts', upload.single('image'),PostController.store);
+router.get('/posts', PostController.index);
+router.post('/posts/:id/like',LikeController.store)
 
 module.exports = router;
